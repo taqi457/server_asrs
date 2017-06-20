6e3ae54bc9130252c6b424ca4ae84b8bd538d190
Adrian Rebmann <adrianrebmann@gmail.com>
2017-06-20 14:45:04 +0200

    fixed test

--- src/test/java/de/p39/asrs/util/reader/KMLReaderTest.java
+++ src/test/java/de/p39/asrs/util/reader/KMLReaderTest.java
@@ -3,6 +3,7 @@ package de.p39.asrs.util.reader;
 import static org.junit.Assert.*;
 
 import java.util.List;
+import java.util.Locale;
 import java.util.Set;
 
 import javax.xml.bind.JAXBException;
@@ -11,6 +12,7 @@ import org.junit.Test;
 
 import de.p39.asrs.server.controller.util.reader.KMLReader;
 import de.p39.asrs.server.model.Coordinate;
+import de.p39.asrs.server.model.LocaleName;
 import de.p39.asrs.server.model.Route;
 import de.p39.asrs.server.model.Site;
 
@@ -44,19 +46,39 @@ public class KMLReaderTest {
 		Set<Site> sites = r.getSites();
 		assertNotNull(sites);
 		assertFalse(sites.isEmpty());
-		Site site = new Site("Museum für Vor- und Frühgeschichte, Schloßstraße, Saarbrücken");
+		Site site = new Site();
+		LocaleName ln = new LocaleName();
+		ln.setLocale(Locale.GERMAN);
+		ln.setString("Museum für Vor- und Frühgeschichte, Schloßstraße, Saarbrücken");
+		site.addLocaleName(ln);
 		site.setCoordinate(new Coordinate(49.2309227,6.9910729));
 		assertTrue(sites.contains(site));
-		site = new Site("Stiftskirche St. Arnual, St.Arnualer Markt, Saarbrücken");
+		site = new Site();
+		ln = new LocaleName();
+		ln.setLocale(Locale.GERMAN);
+		ln.setString("Stiftskirche St. Arnual, St.Arnualer Markt, Saarbrücken");
+		site.addLocaleName(ln);
 		site.setCoordinate(new Coordinate(49.2177141,7.0178181));
 		assertTrue(sites.contains(site));
-		site = new Site("An der Römerbrücke 1, Saarbrücken");
+		site = new Site();
+		ln = new LocaleName();
+		ln.setLocale(Locale.GERMAN);
+		ln.setString("An der Römerbrücke 1, Saarbrücken");
+		site.addLocaleName(ln);
 		site.setCoordinate(new Coordinate(49.2225925,7.02466));
 		assertTrue(sites.contains(site));
-		site = new Site("Römerstadt 1, Saarbrücken");
+		site = new Site();
+		ln = new LocaleName();
+		ln.setLocale(Locale.GERMAN);
+		ln.setString("Römerstadt 1, Saarbrücken");
+		site.addLocaleName(ln);
 		site.setCoordinate(new Coordinate(49.2247862,7.0264679));
 		assertTrue(sites.contains(site));
-		site = new Site("Mithraskapelle am Halberg, Brebacher Landstraße, Saarbrücken");
+		site = new Site();
+		ln = new LocaleName();
+		ln.setLocale(Locale.GERMAN);
+		ln.setString("Mithraskapelle am Halberg, Brebacher Landstraße, Saarbrücken");
+		site.addLocaleName(ln);
 		site.setCoordinate(new Coordinate(49.2225203,7.0283524));
 		assertTrue(sites.contains(site));
 	}
