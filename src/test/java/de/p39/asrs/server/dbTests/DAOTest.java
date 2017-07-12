a92a3debba3ea4ddd0fb65a33af321ed3d58ce06
Adrian Rebmann <adrianrebmann@gmail.com>
2017-07-12 21:42:45 +0200

    removed unnecessary method

--- src/test/java/de/p39/asrs/server/dbTests/DAOTest.java
+++ src/test/java/de/p39/asrs/server/dbTests/DAOTest.java
@@ -9,21 +9,30 @@ import de.p39.asrs.server.controller.db.dao.CategoryDAO;
 import de.p39.asrs.server.controller.db.dao.MediumDAO;
 import de.p39.asrs.server.controller.db.dao.RouteDAO;
 import de.p39.asrs.server.controller.db.dao.SiteDAO;
+import de.p39.asrs.server.model.CategoryType;
 
 public class DAOTest {
 	CrudFacade cf;
 	
 	@Before
 	public void setUp(){
+		System.out.println("Setting up");
 		cf = new JPACrudService("server");
 	}
 	
 	@Test
 	public void roteDAOTest(){
 		RouteDAO dao = new RouteDAO(cf);
+		dao.getAllRoutes();
+		dao.getAllRoutesCompleted();
+		dao.getRouteById(0L);
 	}
 	public void siteDAOTest(){
 		SiteDAO dao = new SiteDAO(cf);
+		dao.getAllSites();
+		dao.getAllSitesCompleted();
+		dao.getSiteById(0L);
+		dao.getSitesByName("");
 	}
 	public void mediaDAOTest(){
 		MediumDAO dao = new MediumDAO(cf);
@@ -37,6 +46,10 @@ public class DAOTest {
 	}
 	public void categoryDAOTest(){
 		CategoryDAO dao = new CategoryDAO(cf);
+		dao.getAllCategories();
+		dao.getCategoriesByType(CategoryType.ROUTE);
+		dao.getCategoriesByType("");
+		dao.getCategoryById(0L);
 	}
 	
 
