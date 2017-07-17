412700f83bc4d44579ca5fdc4fcb9a8507e48695
Robin Daumann <s9rodaum@stud.uni-saarland.de>
2017-07-17 22:31:12 +0200

    tried with http servlet

--- src/main/java/de/p39/asrs/server/controller/MediaController.java
+++ src/main/java/de/p39/asrs/server/controller/MediaController.java
@@ -1,13 +1,16 @@
 package de.p39.asrs.server.controller;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import de.p39.asrs.server.controller.exceptions.BadRequestException;
 import de.p39.asrs.server.model.media.Size;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.http.MediaType;
@@ -19,6 +22,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import de.p39.asrs.server.controller.db.dao.MediumDAO;
 import de.p39.asrs.server.controller.exceptions.InternalServerError;
+
+import javax.servlet.http.HttpServletResponse;
 
 @RestController
 @RequestMapping(value = "/media")
@@ -64,20 +69,26 @@ public class MediaController {
 	}
 
 	@RequestMapping(value = "/audio/example", method = RequestMethod.GET)
-	public FileSystemResource audioExample() {
-		return new FileSystemResource(new File("resources/media/audio/example.mp3"));
+	public void audioExample(HttpServletResponse response) {
+        try {
+            InputStream is = new FileInputStream("resources/media/audio/example.mp3");
+            IOUtils.copy(is, response.getOutputStream());
+            response.flushBuffer();
+        } catch (IOException ex) {
+            throw new InternalServerError("Could not load audio. Contact server admin");
+        }
 	}
 
 	@RequestMapping(value = "/audio/{id}", method = RequestMethod.GET)
-	public FileSystemResource audioByID(@PathVariable Long id) {
-		Path path = Paths.get(dao.getAudioById(id).getPath());
-		return new FileSystemResource(new File(path.toString()));
-		/*try {
-
-					//Files.readAllBytes(path);
-		} catch (IOException e) {
-			throw new InternalServerError("Could not load audio. Contact server admin");
-		}*/
+	public void audioByID(@PathVariable Long id, HttpServletResponse response) {
+	    Path path = Paths.get(dao.getAudioById(id).getPath());
+        try {
+            InputStream is = new FileInputStream("resources/media/audio/example.mp3");
+            IOUtils.copy(is, response.getOutputStream());
+            response.flushBuffer();
+        } catch (IOException ex) {
+            throw new InternalServerError("Could not load audio. Contact server admin");
+        }
 	}
 	
 	@RequestMapping(value = "/video/example", method = RequestMethod.GET, produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
